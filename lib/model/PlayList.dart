5fefdb1ca267e69a571e461fb0fe3be28d522e8d
zaman <zamanrshq@gmail.com>
2023-12-30 09:49:00 +0600

    feat(upgrade): migrate to null safety

--- lib/model/PlayList.dart
+++ lib/model/PlayList.dart
@@ -1,23 +1,25 @@
 class PlayListModel {
   final String uri;
-  final String drm_license_uri;
+  final String drmLicenseUri;
   final String name;
-  final int clip_end_position_ms;
-  final int clip_start_position_ms;
+  final int clipEndPositionMs;
+  final int clipStartPositionMs;
 
-  const PlayListModel(
-      {this.uri,
-      this.drm_license_uri,
-      this.name,
-      this.clip_end_position_ms,
-      this.clip_start_position_ms});
+  const PlayListModel({
+    required this.uri,
+    required this.drmLicenseUri,
+    required this.name,
+    required this.clipEndPositionMs,
+    required this.clipStartPositionMs,
+  });
 
   factory PlayListModel.fromJson(Map<String, dynamic> json) {
     return PlayListModel(
-        uri: json['uri'] as String,
-        drm_license_uri: json['drm_license_uri'] as String,
-        name: json['name'] as String,
-        clip_start_position_ms: json['clip_start_position_ms'] as int,
-        clip_end_position_ms: json['clip_end_position_ms'] as int);
+      uri: json['uri'] as String,
+      drmLicenseUri: json['drm_license_uri'] as String,
+      name: json['name'] as String,
+      clipStartPositionMs: json['clip_start_position_ms'] as int,
+      clipEndPositionMs: json['clip_end_position_ms'] as int,
+    );
   }
 }
