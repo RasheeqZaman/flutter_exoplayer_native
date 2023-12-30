af0516707af147bb8edde70463e031e200a4714b
zaman <zamanrshq@gmail.com>
2023-12-30 10:06:26 +0600

    feat(upgrade): migrate to null safety

--- lib/model/Samples.dart
+++ lib/model/Samples.dart
@@ -2,42 +2,42 @@ import 'package:flutter_app_native/model/PlayList.dart';
 
 class Sample {
   final String uri;
-  final String drmLicenseUri;
+  final String? drmLicenseUri;
   final String name;
-  final String drmScheme;
-  final String adTagUri;
-  final String subtitleUri;
-  final String subtitleMimeType;
-  final String subtitleLanguage;
+  final String? drmScheme;
+  final String? adTagUri;
+  final String? subtitleUri;
+  final String? subtitleMimeType;
+  final String? subtitleLanguage;
   final String extension;
-  final List<PlayListModel> playlist;
+  final List<PlayListModel>? playlist;
 
   const Sample({
-    this.uri,
+    required this.uri,
     this.drmLicenseUri,
-    this.name,
+    required this.name,
     this.drmScheme,
     this.adTagUri,
     this.subtitleUri,
     this.subtitleMimeType,
     this.subtitleLanguage,
-    this.extension,
+    required this.extension,
     this.playlist,
   });
 
   factory Sample.fromJson(Map<String, dynamic> json) {
-    var playListData = (json['playlist'] as List);
+    var playListData = (json['playlist'] as List?);
     return Sample(
-        uri: json['uri'] as String,
-        drmLicenseUri: json['drm_license_uri'] as String,
-        name: json['name'] as String,
-        drmScheme: json['drm_scheme'] as String,
-        adTagUri: json['ad_tag_uri'] as String,
-        subtitleUri: json['subtitle_uri'] as String,
-        subtitleMimeType: json['subtitle_mime_type'] as String,
-        subtitleLanguage: json['subtitle_language'] as String,
-        extension: json['extension'],
-        playlist:
-            playListData?.map((e) => PlayListModel.fromJson(e))?.toList());
+      uri: json['uri'] as String,
+      drmLicenseUri: json['drm_license_uri'] as String?,
+      name: json['name'] as String,
+      drmScheme: json['drm_scheme'] as String,
+      adTagUri: json['ad_tag_uri'] as String,
+      subtitleUri: json['subtitle_uri'] as String,
+      subtitleMimeType: json['subtitle_mime_type'] as String,
+      subtitleLanguage: json['subtitle_language'] as String,
+      extension: json['extension'],
+      playlist: playListData?.map((e) => PlayListModel.fromJson(e)).toList(),
+    );
   }
 }
