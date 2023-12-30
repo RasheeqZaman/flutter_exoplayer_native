4801007a9f741c3c7bfb887f544c8a1968696d56
zaman <zamanrshq@gmail.com>
2023-12-30 09:58:36 +0600

    feat(upgrade): migrate to null safety

--- lib/src/closed_caption_file.dart
+++ lib/src/closed_caption_file.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'sub_rip.dart';
+
 export 'sub_rip.dart' show SubRipCaptionFile;
 
 /// A structured representation of a parsed closed caption file.
@@ -31,16 +32,21 @@ class Caption {
   ///
   /// This is not recommended for direct use unless you are writing a parser for
   /// a new closed captioning file type.
-  const Caption({this.number, this.start, this.end, this.text});
+  const Caption({
+    required this.number,
+    this.start,
+    this.end,
+    required this.text,
+  });
 
   /// The number that this caption was assigned.
   final int number;
 
   /// When in the given video should this [Caption] begin displaying.
-  final Duration start;
+  final Duration? start;
 
   /// When in the given video should this [Caption] be dismissed.
-  final Duration end;
+  final Duration? end;
 
   /// The actual text that should appear on screen to be read between [start]
   /// and [end].
